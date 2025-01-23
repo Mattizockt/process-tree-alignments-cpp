21b26fafb929e31e2bcf493cc90cada73da1ae72
Matthias Garbers <ggurke2@gmail.com>
2025-01-23 19:13:39 +0800

    added default tree structure

---
+++ src/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "TreeNode.h"
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+extern std::unordered_map<std::string, std::unordered_map<std::string, int>> costTable;
+
+#endif // MAIN_H
