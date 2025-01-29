ea27f5cc886bb315fb172cae70202e7bc8d10c93
Matthias Garbers <ggurke2@gmail.com>
2025-01-29 20:49:17 +0800

    implemented initial version of parser for both ptml and xes - refactoring neccessary

--- src/parser.cpp
+++ src/parser.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <filesystem>
 
 std::vector<std::string> parseXes(const std::string &filePath)
@@ -68,58 +69,59 @@ Operation stringToOperation(const std::string &opStr)
     // TODO perhaps add an unknown operation in the future
 }
 
-std::shared_ptr<TreeNode> parsePTMLNode(rapidxml::xml_node<> *xmlNode)
-{
-    if (!xmlNode)
-    {
-        return nullptr;
-    }
-
-    rapidxml::xml_attribute<> *opAttr = xmlNode->first_attribute("operator");
-    rapidxml::xml_attribute<> *actAttr = xmlNode->first_attribute("activity");
-
-    std::shared_ptr<TreeNode> node;
-
-    if (opAttr)
-    {
-        node = std::make_shared<TreeNode>(static_cast<Operation>(stringToOperation(opAttr->value())));
-    }
-    else if (actAttr)
-    {
-        // TODO what happens with silent activities
-        node = std::make_shared<TreeNode>(ACTIVITY, actAttr->value());
-    }
-    else
-    {
-        // TODO throw error?
-        return nullptr;
-    }
-
-    // Recursively process children
-    for (rapidxml::xml_node<> *child = xmlNode->first_node("node"); child; child = child->next_sibling("node"))
-    {
-        std::shared_ptr<TreeNode> childNode = parsePTMLNode(child);
-        if (childNode)
-        {
-            node->addChild(childNode);
-        }
-    }
-
-    return node;
-}
-
 std::shared_ptr<TreeNode> parsePtml(const std::string &filePath)
 {
     rapidxml::file<> xmlFile(filePath.c_str());
     rapidxml::xml_document<> doc;
     doc.parse<0>(xmlFile.data());
 
-    rapidxml::xml_node<> *rootNode = doc.first_node("ptml")->first_node("processTree")->first_node("node");
+    rapidxml::xml_node<> *node = doc.first_node("ptml")->first_node("processTree")->first_node();
     // TODO check if null
-    std::shared_ptr<TreeNode> processTree = parsePTMLNode(rootNode);
 
-    processTree->printTree();
-    return processTree;
+    std::unordered_map<std::string, std::shared_ptr<TreeNode>> idToNode;
+
+    for (rapidxml::xml_node<> *sibling = node; sibling; sibling = sibling->next_sibling())
+    {
+        std::string nodeName = sibling->name();
+
+        if (nodeName == "parentsNode")
+        {
+            // check if element exists
+            std::string parentId = sibling->first_attribute("sourceId")->value();
+            std::string childId = sibling->first_attribute("targetId")->value();
+            idToNode[parentId]->addChild(idToNode[childId]);
+        }
+        else
+        {
+            std::shared_ptr<TreeNode> newNode;
+
+            if (nodeName == "sequence")
+                // operation activity id
+                newNode = std::make_shared<TreeNode>(SEQUENCE, "", sibling->first_attribute("id")->value());
+            else if (nodeName == "and")
+                newNode = std::make_shared<TreeNode>(PARALLEL, "", sibling->first_attribute("id")->value());
+            else if (nodeName == "xor")
+                newNode = std::make_shared<TreeNode>(XOR, "", sibling->first_attribute("id")->value());
+            else if (nodeName == "xorLoop")
+                newNode = std::make_shared<TreeNode>(REDO_LOOP, "", sibling->first_attribute("id")->value());
+            else if (nodeName == "manualTask")
+                newNode = std::make_shared<TreeNode>(ACTIVITY, "", sibling->first_attribute("id")->value());
+            else if (nodeName == "automaticTask")
+                newNode = std::make_shared<TreeNode>(SILENT_ACTIVITY, "", sibling->first_attribute("id")->value());
+            else
+            {
+                throw std::runtime_error("doesn't exist yet");
+            }
+
+            // TODO perhaps add an unknown operation in the future
+            std::cout << newNode->getId();
+            idToNode[newNode->getId()] = newNode;
+        }
+    }
+
+    auto root = idToNode[node->first_attribute("id")->value()];
+    root->printTree();
+    return root;
 }
 
 // TODO use python parser to bring into the correct format
@@ -147,17 +149,19 @@ std::vector<std::string> createPtmlXesPairs()
 
     printVector(fileNames);
 
-    std::string ptmlPath = "../data/ptml/"; 
+    std::string ptmlPath = "../data/ptml/";
     std::vector<std::string> fileEndings = {"_pt00", "_pt10", "_pt25", "_pt50"};
 
-    for (const auto& fileName : fileNames) {
-        for (const auto& fileEnding : fileEndings) { 
+    for (const auto &fileName : fileNames)
+    {
+        for (const auto &fileEnding : fileEndings)
+        {
             const std::string filePtmlPath = ptmlPath + fileName + fileEnding + ".ptml";
             const std::string fileXesPath = xesPath + fileName + ".xes";
 
             auto processTree = parsePtml(filePtmlPath);
             auto trace = parseXes(fileXesPath);
-            
+
             processTree->printTree();
             printVector(trace);
         }
