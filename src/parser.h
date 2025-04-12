fe58b5ca85b69d09519c949802a6c5114109fdd1
Matthias Garbers <ggurke2@gmail.com>
2025-04-12 20:18:32 +0800

    substitued vector for trace with span

--- src/parser.h
+++ src/parser.h
@@ -7,7 +7,6 @@
 
 extern std::unordered_map<std::string, int> activitiesToInt;
 extern std::vector<std::string> activityVector;
-extern std::atomic<bool> stopFlag;
 
 std::vector<std::vector<int>> parseXes(const std::string &filePath);
 std::shared_ptr<TreeNode> parsePtml(const std::string &filePath);
